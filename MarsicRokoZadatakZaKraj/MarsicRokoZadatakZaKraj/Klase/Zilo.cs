47231ea59a7ad2fddeb0db7cd0c4f39f68e2afcf
Roko Maršić <45421234+plaut1337@users.noreply.github.com>
2019-02-27 18:10:01 +0100

    Add files via upload

---
+++ MarsicRokoZadatakZaKraj/MarsicRokoZadatakZaKraj/Klase/Zilo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsicRokoZadatakZaKraj.Klase
+{
+    class Zilo
+    {
+        string sta;
+        double maxZina;
+        int brTaca;
+
+        public Zilo (string sta, double maxZina, int brTaca)
+        {
+            this.sta = sta;
+            this.maxZina = maxZina;
+            this.brTaca = brTaca;
+        }
+        public string ispis()
+        {
+            return "Vrsta: " + sta + "\nMax brzina: " + maxZina.ToString() + "\nBroj kotaca: " + brTaca.ToString();
+        }
+        public Zilo() { }
+
+    }
+}
---
+++ MarsicRokoZadatakZaKraj/MarsicRokoZadatakZaKraj/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarsicRokoZadatakZaKraj.Klase;
+
+namespace MarsicRokoZadatakZaKraj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sta;
+            int brTaca;
+            double maxZina;
+            Console.WriteLine("Vrsta: ");
+            sta = Console.ReadLine();
+            Console.WriteLine("\nMax brzina: ");
+            maxZina = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nBroj kotaca: ");
+            brTaca = Convert.ToInt32(Console.ReadLine());
+            Zilo cikl = new Zilo(sta, maxZina, brTaca);
+            Zilo auto = new Zilo(sta, (maxZina*1.3), (brTaca*2));
+            Console.WriteLine(cikl.ispis());
+            Console.WriteLine(auto.ispis());
+            Console.ReadKey();
+        }
+    }
+}
